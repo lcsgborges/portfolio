d029bc4c9636965aff2ab0f495190a9ab19f7d90
lucas <lcsgborges@gmail.com>
2025-05-02 18:34:54 -0300

    feat: sobre mim e meu projetos (incompleto)

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll(".header-container ul li a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
