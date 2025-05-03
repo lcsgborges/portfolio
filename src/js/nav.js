5768f8547acc815958c2640f2a7b52c162e0c9ce
lucas <lcsgborges@gmail.com>
2025-05-03 00:50:25 -0300

    feat: responsividade em habilidades

--- src/js/nav.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll(".header-container ul li a");
-
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.forEach(l => l.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
